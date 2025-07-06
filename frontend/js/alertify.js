65c68ed68782d610bbf4b4230ebba983e249b21c
unknown <->
2025-07-06 18:40:34 +0200

    change spaces to tabs

--- frontend/js/alertify.js
+++ frontend/js/alertify.js
@@ -1,26 +1,26 @@
 const mainMsgDiv = document.getElementById('messageMainDiv');
 
 function alertify(message, type, duration = 6000) {
-    let color;
-    switch (type) {
-        case 'success':
-            color = 'success';
-            break;
-        case 'error':
-            color = 'error';
-            break;
-        default:
-            color = 'warning';
-            break;
-    }
-    const msgNode = createElement("div", { "class": `alertify_message ${color}` });
-    msgNode.innerHTML = `<span class="alertify_message_text">${message}</span>`;
-    const timeOut = setTimeout(() => {
-        msgNode.remove();
-    }, duration);
-    msgNode.addEventListener('click', () => {
-        msgNode.remove();
-        clearTimeout(timeOut);
-    });
-    mainMsgDiv.appendChild(msgNode);
+	let color;
+	switch (type) {
+		case 'success':
+			color = 'success';
+			break;
+		case 'error':
+			color = 'error';
+			break;
+		default:
+			color = 'warning';
+			break;
+	}
+	const msgNode = createElement("div", { "class": `alertify_message ${color}` });
+	msgNode.innerHTML = `<span class="alertify_message_text">${message}</span>`;
+	const timeOut = setTimeout(() => {
+		msgNode.remove();
+	}, duration);
+	msgNode.addEventListener('click', () => {
+		msgNode.remove();
+		clearTimeout(timeOut);
+	});
+	mainMsgDiv.appendChild(msgNode);
 }
--- frontend/js/router.js
+++ frontend/js/router.js
@@ -7,21 +7,21 @@ const loader = document.getElementById('loader');
 // key is the url route
 // value is the html file to load
 const routes = {
-  '/': 'pages/home.html',
+	'/': 'pages/home.html',
 }
 
 // assets and page title associated with each html file
 const assets = new Map([
-  ['pages/home.html', {
-    js: ["js/pages/home.js"],
-    css: ["css/pages/home.css"],
-    title: "Home Page"
-  }],
-  ['pages/404.html', {
-    js: [],
-    css: ["css/pages/404.css"],
-    title: "404"
-  }],
+	['pages/home.html', {
+		js: ["js/pages/home.js"],
+		css: ["css/pages/home.css"],
+		title: "Home Page"
+	}],
+	['pages/404.html', {
+		js: [],
+		css: ["css/pages/404.css"],
+		title: "404"
+	}],
 
 ]);
 
@@ -38,56 +38,56 @@ DO NOT MESS WITH CODE BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING
 // Every time page loads, it checks the url in session storage and loads the corresponding html file.
 // If no saved url is found, it loads the home page.
 document.addEventListener('click', (e) => {
-  const target = e.target.closest('[data-link]');
-  if (target) {
-    e.preventDefault();
-    const href = target.getAttribute('href');
-    sessionStorage.setItem('lastPath', href);
-    location.reload();
-  }
+	const target = e.target.closest('[data-link]');
+	if (target) {
+		e.preventDefault();
+		const href = target.getAttribute('href');
+		sessionStorage.setItem('lastPath', href);
+		location.reload();
+	}
 });
 
 
 waitForPageLoad().then(async () => {
-  const storedPath = sessionStorage.getItem('lastPath') || '/';
-  setActiveNavLink(storedPath);
+	const storedPath = sessionStorage.getItem('lastPath') || '/';
+	setActiveNavLink(storedPath);
 
-  const view = routes[storedPath] || 'pages/404.html';
-  const asset = assets.get(view);
-  loader.classList.remove('hide');
+	const view = routes[storedPath] || 'pages/404.html';
+	const asset = assets.get(view);
+	loader.classList.remove('hide');
 
 
-  try {
-    const res = await fetch(view);
-    if (!res.ok) throw new Error(`Failed to load ${view}`);
-    const html = await res.text();
-    mainContentElement.innerHTML = html;
+	try {
+		const res = await fetch(view);
+		if (!res.ok) throw new Error(`Failed to load ${view}`);
+		const html = await res.text();
+		mainContentElement.innerHTML = html;
 
-    document.title = asset?.title || "Wails";
+		document.title = asset?.title || "Wails";
 
-    if (asset?.css?.length) {
-      asset.css.forEach(href => {
-        const link = document.createElement('link');
-        link.rel = 'stylesheet';
-        link.href = href;
-        document.head.appendChild(link);
-      });
-    }
+		if (asset?.css?.length) {
+			asset.css.forEach(href => {
+				const link = document.createElement('link');
+				link.rel = 'stylesheet';
+				link.href = href;
+				document.head.appendChild(link);
+			});
+		}
 
-    if (asset?.js?.length) {
-      for (const script of asset.js) {
-        const s = document.createElement('script');
-        s.src = script;
-        s.type = 'module';
-        document.body.appendChild(s);
-      }
-    }
-    // needed for waitForComponent() function to work
-    window.loadedPage = true;
-  } catch (err) {
-    console.error(err);
-    mainEl.innerHTML = `<p>Error: ${err.message}</p>`;
-  } finally {
-    loader.classList.add('hide');
-  }
+		if (asset?.js?.length) {
+			for (const script of asset.js) {
+				const s = document.createElement('script');
+				s.src = script;
+				s.type = 'module';
+				document.body.appendChild(s);
+			}
+		}
+		// needed for waitForComponent() function to work
+		window.loadedPage = true;
+	} catch (err) {
+		console.error(err);
+		mainEl.innerHTML = `<p>Error: ${err.message}</p>`;
+	} finally {
+		loader.classList.add('hide');
+	}
 });
